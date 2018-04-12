5e53bc5386faf08126882978bd528617a9de8447
t-linwuhua <t-linwuhua@megvii.com>
2018-04-12 14:59:09 +0800

    'AspectRatio'

--- app/src/main/java/com/klgz/xibao/ui/activity/RecyclerViewActivity.java
+++ app/src/main/java/com/klgz/xibao/ui/activity/RecyclerViewActivity.java
@@ -6,17 +6,12 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.github.jdsjlzx.recyclerview.LRecyclerView;
-import com.github.jdsjlzx.recyclerview.LRecyclerViewAdapter;
 import com.klgz.xibao.R;
 import com.klgz.xibao.ui.fragment.PermissionsFragment;
 import com.klgz.xibao.utils.LocationUtils;
@@ -29,33 +24,16 @@ import java.util.List;
  */
 
 public class RecyclerViewActivity extends AppCompatActivity{
-    private LRecyclerView mRecyclerView;
-    private LRecyclerViewAdapter mLRecyclerViewAdapter;
     private List<String> datas=new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler_view);
 
 //        recyclerAndLocation();
     }
 
     private void recyclerAndLocation() {
-        mRecyclerView= (LRecyclerView) findViewById(R.id.recyclerView);
-        StaggeredGridLayoutManager manager=new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
-        manager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mLRecyclerViewAdapter = new LRecyclerViewAdapter(new CustomAdapter());
-        mRecyclerView.setAdapter(mLRecyclerViewAdapter);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
-
-//        for (int i = 0; i < 3; i++) {
-//            datas.add("item:"+i);
-//
-//        }
-        mLRecyclerViewAdapter.notifyDataSetChanged();
-
         PermissionsFragment.getInstance(this).requestPermissions(new PermissionsFragment.PermissionsCallback() {
             @Override
             public void requestSuccess() {
@@ -65,7 +43,6 @@ public class RecyclerViewActivity extends AppCompatActivity{
                     @Override
                     public void onLocation(Location location) {
                         datas.add("onLocationChanged"+"\n"+location.toString()+"\n"+LocationUtils.getAddress(RecyclerViewActivity.this,location.getLatitude(),location.getLongitude()).toString());
-                        mLRecyclerViewAdapter.notifyDataSetChanged();
                     }
                 });
 
